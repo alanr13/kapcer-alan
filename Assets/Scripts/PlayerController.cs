12ddac71cd539b2031d37aec881806fe79ffc3b1
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-02-24 18:32:02 +0100

    Added project files to repository

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float jumpForce = 10f;
+    public float maxSpeed = 5f;
+
+    private Rigidbody2D rb;
+    Vector2 moveDirection = Vector2.zero;
+    private bool isJumping = false;
+    private bool isGrounded;
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(rb.linearVelocity.x) < maxSpeed)
+        {
+            rb.AddForce(new Vector2(moveDirection.x * moveSpeed, 0), ForceMode2D.Force);
+        }
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = false;
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
